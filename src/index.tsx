d84bfb324396900ac6640aac5524cd4e8bc67688
yuruttoiyashi <yutanpo.fm@gmail.com>
2026-02-13 08:18:31 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // ここで佐藤さんの完璧なApp.tsxを呼び出します
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
