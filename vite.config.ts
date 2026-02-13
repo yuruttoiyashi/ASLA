0f68c213b3ac29578c1ce3138536728de858f104
yuruttoiyashi <yutanpo.fm@gmail.com>
2026-02-13 09:27:42 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // リポジトリ名が「ASLA」なら、前後をスラッシュで囲むのが正解です
+  // GitHub Pagesのサブディレクトリ名「/ASLA/」を正確に伝えます
   base: '/ASLA/', 
   build: {
+    // 部品をまとめるフォルダ名を指定します
     outDir: 'dist',
   }
 })
